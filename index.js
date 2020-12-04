4351c2f0d4cb8650e6bcd0e1a0f04b1644ca8f07
Cody Cronan <codycronberger@gmail.com>
2020-12-04 13:31:05 -0800

    added simple movie api get

---
+++ index.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+let search = "The";
+const IMDB_URL = `https://imdb-api.com/en/API/SearchMovie/${process.env.IMDB_KEY}/${search}`;
+
+  axios.get(IMDB_URL).then(res => {
+   
+    const movies = res.data.results;
+    movies.forEach(movie => {
+      console.log(movie.title);
+    })
+
+  }).catch(err => {
+    console.log(err);
+  })
